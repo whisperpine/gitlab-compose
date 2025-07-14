04c3099f92763a7933cb7242ebbc71e12de94d83
Yusong <yusonglai64@gmail.com>
2025-07-14 21:02:23 +0800

    chore: upgrade cloudflare provider to "~> 5.6"

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -5,7 +5,7 @@ terraform {
   required_providers {
     cloudflare = {
       source  = "cloudflare/cloudflare"
-      version = "~> 5.3"
+      version = "~> 5.6"
     }
     random = {
       source  = "hashicorp/random"
